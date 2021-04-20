0fd5afa805bf9757b78c2bb52a184058a1d071e3
ozairjunior <ozairjunior23@outlook.com>
2021-04-20 18:00:18 -0300

    atualização na view

---
+++ intro-spring/intro-spring/src/main/java/br/com/unipe/aula/config/SpringMvcConfig.java
@@ -0,0 +1,20 @@
+package br.com.unipe.aula.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+public class SpringMvcConfig {
+	
+	@Bean
+	public InternalResourceViewResolver viewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+		resolver.setViewClass(JstlView.class);
+		
+		return resolver;
+	}
+}
---
+++ intro-spring/intro-spring/src/main/java/br/com/unipe/aula/web/controller/WelcomeController.java
@@ -0,0 +1,36 @@
+package br.com.unipe.aula.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.unipe.aula.model.Moedas;
+
+@Controller
+public class WelcomeController {
+	
+	@RequestMapping(value= "/formulario", method = RequestMethod.GET)
+	public ModelAndView formulario(Model model) {
+		model.addAttribute("moedas", new Moedas());
+		return new ModelAndView("formulario");
+	}
+	
+	
+	@RequestMapping(value= "/formulario", method = RequestMethod.POST)
+	public ModelAndView exibelocal(@ModelAttribute Moedas moedas) {
+		ModelAndView view = new ModelAndView("formulario");
+		
+		view.addObject("dolar","1 real equivale a: $"+ moedas.getDolar_cota());
+		
+		view.addObject("real","1 dolar equivale a: R$"+ moedas.getReal_cota());
+		
+		view.addObject("ContaDolar","O valor em Dolares é: " + moedas.resultadoDolar());
+		view.addObject("ContaReal","O valor em Reais é: " + moedas.resultadoReal());
+		
+		return view;
+		
+	}
+}
